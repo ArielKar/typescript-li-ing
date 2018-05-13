e6cc160cb2b3e88f75b8b359f61d1369c069281e
ArielK <arielkarmazin@gmail.com>
2018-05-13 16:20:12 +0300

    adding color

--- dist/list-view.js
+++ dist/list-view.js
@@ -5,7 +5,12 @@ var ListView = /** @class */ (function () {
     ListView.prototype.add = function (value) {
         var newLi = document.createElement("li");
         newLi.innerText = value;
+        console.log(this.elem.style.color);
+        newLi.style.color = "rgb(" + this.getRandom() + "," + this.getRandom() + "," + this.getRandom() + ")";
         this.elem.appendChild(newLi);
+    };
+    ListView.prototype.getRandom = function () {
+        return Math.floor((Math.random() * 255) + 1);
     };
     return ListView;
 }());
--- list-view.ts
+++ list-view.ts
@@ -5,6 +5,12 @@ class ListView {
     add(value) {
         let newLi = document.createElement("li");
         newLi.innerText = value;
+        console.log(this.elem.style.color);
+        newLi.style.color = `rgb(${this.getRandom()},${this.getRandom()},${this.getRandom()})`;
         this.elem.appendChild(newLi);
+    }
+
+    getRandom() {
+        return Math.floor((Math.random()*255)+1);
     }
 }
